f1a004b00f136b01e1073a362ad08189e54a2250
absa07002 <absa07002@DESKTOP-TAGHD5A>
2024-12-17 13:54:17 +0100

    Add project files.

---
+++ inlämning nivå 6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Talen från 10 till 1:");
+
+        
+        for (int i = 10; i >= 1; i--)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
